c46cbcd89fc2d6c66bec568e4897712a4a906600
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-02-25 23:43:07 +0530

    routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,6 @@
-// const express = require("express");
-// const userRouter = require("./user");
-// const router = express.Router();
-
-// router.use("/user", userRouter);
-// module.exports = router;
 const express = require("express");
 const userRouter = require("./user");
 
 const router = express.Router();
-router.use("api/v1/user", userRouter);
+router.use("/user", userRouter);
 module.exports = router;
