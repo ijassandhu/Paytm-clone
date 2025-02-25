dc2addbc0703ebc06763f155ac0ae20c0a9249f2
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-02-25 23:46:22 +0530

    zod validation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+
+const schema = zod.object({
+  username: zod.string(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
+});
+router.post("/api/v1/signup", (req, res) => {});
+router.get("/api/v1/signin", (req, res) => {});
 module.exports = {
   router,
 };
