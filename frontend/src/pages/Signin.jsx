de493bd36e6b20ee43e36449286d8dae470b6aa1
Jaskeerat <jaskeeratsingh219@gmail.com>
2025-03-10 23:54:55 +0530

    added signin

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,21 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../components/Heading";
 import SubHeading from "../components/SubHeading";
 import InputBox from "../components/InputBox";
 import Button from "../components/Button";
 import BottomWarning from "../components/BottomWarning";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    console.log("Sign-in button clicked"); // ✅ Check if event fires
+
+    // try {
+    const { data } = await axios.post(
+      "http://localhost:3000/api/v1/user/signin",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log("Response from server:", data); // ✅ Log response
+    localStorage.setItem("token", data.token);
+    navigate("/dashboard");
+    // } catch (err) {
+    //   console.log("Error:", err.response ? err.response.data : err.message);
+    // }
+  };
+
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
-        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max `px-4">
           <Heading label={"Sign In"} />
           <SubHeading label={"Enter your credentials to access your account"} />
-          <InputBox placeholder="jaskeerat@gmail.com" label={"Email"} />
-          <InputBox placeholder="123456" label={"Password"} />
+          <InputBox
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="jaskeerat@gmail.com"
+            label={"Email"}
+          />
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="#jas1234"
+            label={"Password"}
+          />
           <div className="pt-4">
-            <Button label={"Sign in"} />
+            <Button onClick={handleSignin} label={"Sign in"} />
           </div>
           <BottomWarning
             label={"Don't have an account?"}
